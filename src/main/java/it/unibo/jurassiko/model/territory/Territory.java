080aff9240cec62d84676e57083472554acebcb0
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-01-16 15:34:58 +0100

    Add Territory interface

---
+++ src/main/java/it/unibo/jurassiko/model/territory/Territory.java
@@ -0,0 +1,23 @@
+package it.unibo.jurassiko.model.territory;
+
+import java.util.Set;
+
+public interface Territory {
+
+    String getName();
+
+    String getContinent();
+
+    // Player getOwner();
+
+    Set<String> getNeighbourNames();
+
+    void setNeighbours(Set<Territory> neighbours);
+
+    Set<Territory> getNeighbours();
+
+    void changeDinoAmount(int delta);
+
+    int getDinoAmount();
+
+}
