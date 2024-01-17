4e89030b37639e33c3de51e0c712a7693ae568bc
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-01-17 20:36:02 +0100

    Add AbstractObjective class

---
+++ src/main/java/it/unibo/jurassiko/model/objective/impl/AbstractObjective.java
@@ -0,0 +1,27 @@
+package it.unibo.jurassiko.model.objective.impl;
+
+import it.unibo.jurassiko.model.objective.api.Objective;
+
+public abstract class AbstractObjective implements Objective {
+
+    private final static String DEFAULT_OBJECTIVE_DESCRIPTION = "Conquista 12 territori e 2 oceani.";
+
+    protected String description = "";
+    private boolean achieved = false;
+
+    @Override
+    public boolean isAchieved() {
+        return achieved;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void writeDescription();
+
+    protected String getDefaultObjectiveDescription() {
+        return DEFAULT_OBJECTIVE_DESCRIPTION;
+    }
+}
