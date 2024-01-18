b3f9b283afba246b8a8df2250804108abe17c196
Botti Bao <botti.bao@studio.unibo.it>
2024-01-18 17:51:39 +0100

    Removed magic number

--- src/main/java/it/unibo/jurassiko/model/dice/impl/DiceImpl.java
+++ src/main/java/it/unibo/jurassiko/model/dice/impl/DiceImpl.java
@@ -6,12 +6,13 @@ import it.unibo.jurassiko.model.dice.api.Dice;
 
 public class DiceImpl implements Dice{
 
+    private static final int DICE_FACES = 6;
     //take the ran seed with the current time to make it as random as possible
     private final Random random = new Random(System.currentTimeMillis());
 
     @Override
     public int roll() {
-        return random.nextInt(6) + 1;
+        return random.nextInt(DICE_FACES) + 1;
     }
     
 }
