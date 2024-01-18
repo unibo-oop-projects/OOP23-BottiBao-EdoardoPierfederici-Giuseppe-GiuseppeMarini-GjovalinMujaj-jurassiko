21dd0dbc74c93350a5566c518a4fefa6d3157114
Botti Bao <botti.bao@studio.unibo.it>
2024-01-18 16:34:56 +0100

    Finished Dice

--- src/main/java/it/unibo/jurassiko/model/dice/impl/DiceImpl.java
+++ src/main/java/it/unibo/jurassiko/model/dice/impl/DiceImpl.java
@@ -1,5 +1,17 @@
-package it.unibo.jurassiko.model.dice;
+package it.unibo.jurassiko.model.dice.impl;
 
-public class Dice {
+import java.util.Random;
+
+import it.unibo.jurassiko.model.dice.api.Dice;
+
+public class DiceImpl implements Dice{
+
+    //take the ran seed with the current time to make it as random as possible
+    private final Random random = new Random(System.currentTimeMillis());
+
+    @Override
+    public int roll() {
+        return random.nextInt(6) + 1;
+    }
     
 }
