79f7adf0ad6e00a9c3efb6a865b9743a0b1e578b
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-01-18 11:11:30 +0100

    Add Jackson tags to Objective interface for JSON parsing purposes

--- src/main/java/it/unibo/jurassiko/model/objective/api/Objective.java
+++ src/main/java/it/unibo/jurassiko/model/objective/api/Objective.java
@@ -1,5 +1,19 @@
 package it.unibo.jurassiko.model.objective.api;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+import it.unibo.jurassiko.model.objective.impl.ConquerContinentsObjective;
+import it.unibo.jurassiko.model.objective.impl.ConquerTerritoriesObjective;
+import it.unibo.jurassiko.model.objective.impl.DestroyArmyObjective;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @Type(value = ConquerTerritoriesObjective.class, name = "conquerTerritories"),
+        @Type(value = DestroyArmyObjective.class, name = "destroyArmy"),
+        @Type(value = ConquerContinentsObjective.class, name = "conquerContinents")
+})
 public interface Objective {
 
     /**
