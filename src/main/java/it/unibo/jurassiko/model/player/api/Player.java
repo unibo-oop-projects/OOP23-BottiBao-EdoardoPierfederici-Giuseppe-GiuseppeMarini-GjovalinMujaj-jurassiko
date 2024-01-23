30393fa38ee8d82706aa0836678fab04d7d91c67
Botti Bao <botti.bao@studio.unibo.it>
2024-01-23 02:13:30 +0100

    template for the Player class

--- src/main/java/it/unibo/jurassiko/model/player/api/Player.java
+++ src/main/java/it/unibo/jurassiko/model/player/api/Player.java
@@ -1,13 +1,21 @@
 package it.unibo.jurassiko.model.player.api;
 
+import java.util.Set;
+
 public interface Player {
+
     enum PlayerColor {
         Color1, Color2
     }
 
     PlayerColor getColor();
 
-    // ? getObjective();
+    Objective getObjective();
 
-    
+    Set<Territorie> getOwnedTerritories();
+
+    Set<Continent> getOwnedContinents();
+
+    // ??? getBonusHand();
+
 }
