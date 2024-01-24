ee6b135a0c110d52651ab26b6fa9a59639ad14dc
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-01-24 18:49:37 +0100

    Add new methods to Ocean interface

--- src/main/java/it/unibo/jurassiko/model/ocean/api/Ocean.java
+++ src/main/java/it/unibo/jurassiko/model/ocean/api/Ocean.java
@@ -2,17 +2,27 @@ package it.unibo.jurassiko.model.ocean.api;
 
 import java.util.Set;
 
+import it.unibo.jurassiko.model.territory.api.Territory;
+
 public interface Ocean {
 
     String getName();
 
     Set<String> getNeighbourNames();
 
+    Set<String> getAdjTerritoryNames();
+
     void setNeighbours(Set<Ocean> neighbours);
+
+    void setAdjTerritories(Set<Territory> adjTerritories);
 
     Set<Ocean> getNeighbours();
 
+    Set<Territory> getAdjTerritories();
+
     boolean isNeighbour(String oceanName);
+
+    boolean isAdjTerritory(String territoryName);
 
     // changeDinoAmount()
 
