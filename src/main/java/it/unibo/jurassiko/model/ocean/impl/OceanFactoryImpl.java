231b7ffb30962f371ee0df69e243abfcab2a74a8
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-01-24 18:53:19 +0100

    Add OceanFactoryImpl class

---
+++ src/main/java/it/unibo/jurassiko/model/ocean/impl/OceanFactoryImpl.java
@@ -0,0 +1,22 @@
+package it.unibo.jurassiko.model.ocean.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import it.unibo.jurassiko.model.ocean.api.Ocean;
+import it.unibo.jurassiko.model.ocean.api.OceanFactory;
+import it.unibo.jurassiko.reader.api.JSONFileReader;
+import it.unibo.jurassiko.reader.impl.OceanReader;
+
+public class OceanFactoryImpl implements OceanFactory {
+
+    private static final String PATH = "oceans.json";
+
+    @Override
+    public Set<Ocean> createOceans() {
+        final JSONFileReader<Ocean> oceanReader = new OceanReader();
+        Set<Ocean> oceans = oceanReader.readFileData(PATH);
+        return new HashSet<>(oceans);
+    }
+
+}
