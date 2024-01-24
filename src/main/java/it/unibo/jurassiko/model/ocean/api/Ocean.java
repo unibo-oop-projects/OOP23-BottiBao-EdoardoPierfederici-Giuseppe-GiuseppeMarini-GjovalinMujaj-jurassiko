232303d7c805a967a0d0e6998944c192270eaa3c
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-01-24 19:18:37 +0100

    Fix: add JsonDeserialize tag to Ocean interface for parsing

--- src/main/java/it/unibo/jurassiko/model/ocean/api/Ocean.java
+++ src/main/java/it/unibo/jurassiko/model/ocean/api/Ocean.java
@@ -2,8 +2,12 @@ package it.unibo.jurassiko.model.ocean.api;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import it.unibo.jurassiko.model.ocean.impl.OceanImpl;
 import it.unibo.jurassiko.model.territory.api.Territory;
 
+@JsonDeserialize(as = OceanImpl.class)
 public interface Ocean {
 
     String getName();
