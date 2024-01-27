cea0ef2071427394a251a05f7d68eb670a482213
Botti Bao <botti.bao@studio.unibo.it>
2024-01-27 11:48:07 +0100

    Mod TestDice repository

--- src/test/java/it/unibo/jurassiko/model/TestDice.java
+++ src/test/java/it/unibo/jurassiko/model/TestDice.java
@@ -1,4 +1,4 @@
-package it.unibo.jurassiko;
+package it.unibo.jurassiko.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
