d78bbfa792b18dbdc33435492465c12ee2bfae43
Botti Bao <botti.bao@studio.unibo.it>
2024-01-27 01:49:50 +0100

    Add JavaDoc for Dice and DiceImpl

--- src/main/java/it/unibo/jurassiko/model/dice/api/Dice.java
+++ src/main/java/it/unibo/jurassiko/model/dice/api/Dice.java
@@ -1,6 +1,12 @@
 package it.unibo.jurassiko.model.dice.api;
 
+/**
+ * Interface for the Dice
+ */
 public interface Dice {
 
+    /**
+     * @return a random number between 1 to 6
+     */
     int roll();
 }
