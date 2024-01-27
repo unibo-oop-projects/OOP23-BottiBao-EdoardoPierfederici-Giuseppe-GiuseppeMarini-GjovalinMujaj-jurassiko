60f3e2b4f678320d8e8514556e4f8e18553b41cf
Botti Bao <botti.bao@studio.unibo.it>
2024-01-27 02:35:38 +0100

    Add Player interface, PlayerImpl Class and TestPlayer

--- src/main/java/it/unibo/jurassiko/model/player/api/Player.java
+++ src/main/java/it/unibo/jurassiko/model/player/api/Player.java
@@ -2,19 +2,82 @@ package it.unibo.jurassiko.model.player.api;
 
 import java.util.Set;
 
+/**
+ * Interface who provide method for the Player
+ */
 public interface Player {
+    /**
+     * Enum for the colors
+     */
+    enum Color {
+        RED("Red", 255, 0, 0),
+        GREEN("Green", 0, 255, 0),
+        BLUE("Blue", 0, 0, 255);
 
-    enum PlayerColor {
-        Color1, Color2
+        private final String color;
+        private final int red;
+        private final int green;
+        private final int blue;
+
+        /**
+         * Constructor for the Colors
+         * 
+         * @param color       color name
+         * @param RedAmount   red color amount
+         * @param GreenAmount green color amount
+         * @param BlueAmount  blue color amount
+         */
+        private Color(final String color, final int RedAmount, final int GreenAmount, final int BlueAmount) {
+            this.color = color;
+            this.red = RedAmount;
+            this.green = GreenAmount;
+            this.blue = BlueAmount;
+        }
+
+        /**
+         * @return color name
+         */
+        public String getColor() {
+            return color;
+        }
     }
 
-    PlayerColor getColor();
+    /**
+     * @return player color
+     */
+    Color getPlayerColor();
 
+    /**
+     * @param objective player objective
+     */
+    void setPlayerObjective(Objective objective);
+
+    /**
+     * @return player objective
+     */
     Objective getObjective();
 
-    Set<Territorie> getOwnedTerritories();
+    /**
+     * @return {@code true} if the objective is completed, {@code false} if otherwise
+     */
+    boolean isObjectiveComplete();
 
-    Set<Continent> getOwnedContinents();
+    /**
+     * @param territorie the territory to add
+     */
+    void addPlayerTerritory(Territory territorie);
+
+    /**
+     * @param territorie the territory to remove
+     */
+    void removePlayerTerritory(Territory territorie);
+
+    /**
+     * @return set of player's territories
+     */
+    Set<Territory> getOwnedTerritories();
+
+    //Set<Continent> getOwnedContinents();
 
     // ??? getBonusHand();
 
--- src/main/java/it/unibo/jurassiko/model/player/impl/PlayerImpl.java
+++ src/main/java/it/unibo/jurassiko/model/player/impl/PlayerImpl.java
@@ -1,36 +1,8 @@
 package it.unibo.jurassiko.model.player.impl;
+/**
+ * PlayerImpl
+ */
+public class PlayerImpl {
 
-import java.util.Set;
-
-import it.unibo.jurassiko.model.player.api.Continent;
-import it.unibo.jurassiko.model.player.api.Objective;
-import it.unibo.jurassiko.model.player.api.Player;
-import it.unibo.jurassiko.model.player.api.Territorie;
-
-public class PlayerImpl implements Player{
-
-    @Override
-    public PlayerColor getColor() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getColor'");
-    }
-
-    @Override
-    public Objective getObjective() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getObjective'");
-    }
-
-    @Override
-    public Set<Territorie> getOwnedTerritories() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOwnedTerritories'");
-    }
-
-    @Override
-    public Set<Continent> getOwnedContinents() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOwnedContinents'");
-    }
     
 }
