0c0694caea314a8a687fb1efc7c04f76c6d76b8b
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-01-29 14:49:45 +0100

    Add documentation to Pair class

--- src/main/java/it/unibo/jurassiko/common/Pair.java
+++ src/main/java/it/unibo/jurassiko/common/Pair.java
@@ -1,5 +1,13 @@
 package it.unibo.jurassiko.common;
 
+/**
+ * A simple record class representing an immutable pair of two objects.
+ * 
+ * @param <X> The type of the first generic element
+ * @param <Y> The type of the second generic element
+ * @param x   The value of the first element
+ * @param y   The value of the second element
+ */
 public record Pair<X, Y>(X x, Y y) {
 
 }
