404510911e834232218e33db5b4d8484bf383ff5
Botti Bao <botti.bao@studio.unibo.it>
2024-01-30 11:22:43 +0100

    Mod Dice Javadoc

--- src/main/java/it/unibo/jurassiko/model/dice/api/Dice.java
+++ src/main/java/it/unibo/jurassiko/model/dice/api/Dice.java
@@ -8,13 +8,17 @@ import java.util.List;
 public interface Dice {
 
     /**
+     * Roll the dice
+     * 
      * @return a random number between 1 to 6
      */
     int roll();
 
     /**
+     * Rolls the dice for 'amount' of times
+     * 
      * @param amount is the amount of rolls
-     * @return {@code List} of integers
+     * @return a list of integers
      */
     List<Integer> rollMultiple(int amount);
 }
