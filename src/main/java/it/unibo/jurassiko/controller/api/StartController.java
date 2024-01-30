4157ea90bba0ec3cdeb21f2bac4145ad840b96b2
Botti Bao <botti.bao@studio.unibo.it>
2024-01-30 22:20:38 +0100

    Add Javadoc

--- src/main/java/it/unibo/jurassiko/controller/api/StartController.java
+++ src/main/java/it/unibo/jurassiko/controller/api/StartController.java
@@ -1,10 +1,22 @@
 package it.unibo.jurassiko.controller.api;
 
+/**
+ * Interface for the Starting Menu.
+ */
 public interface StartController {
-    
+
+    /**
+     * Start the Main Menu view.
+     */
     void startView();
 
+    /**
+     * Start the game.
+     */
     void startGame();
 
+    /**
+     * Close the Main Menu View.
+     */
     void quitView();
 }
