2128b659c41272ab2b5b249dec5b19afb52e2d77
Botti Bao <botti.bao@studio.unibo.it>
2024-01-30 22:18:03 +0100

    Add StartController Class

---
+++ src/main/java/it/unibo/jurassiko/controller/impl/StartContollerImpl.java
@@ -0,0 +1,25 @@
+package it.unibo.jurassiko.controller.impl;
+
+import it.unibo.jurassiko.controller.api.StartController;
+
+public class StartContollerImpl implements StartController {
+
+    @Override
+    public void startView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startView'");
+    }
+
+    @Override
+    public void startGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startGame'");
+    }
+
+    @Override
+    public void quitView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quitView'");
+    }
+    
+}
