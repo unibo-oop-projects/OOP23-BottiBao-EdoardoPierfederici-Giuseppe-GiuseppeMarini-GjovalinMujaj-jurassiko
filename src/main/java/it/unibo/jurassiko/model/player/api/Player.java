14175b4ede341f40b021cedd2213de27460746b5
Botti Bao <botti.bao@studio.unibo.it>
2024-01-31 22:08:52 +0100

    Add Player interface methods

--- src/main/java/it/unibo/jurassiko/model/player/api/Player.java
+++ src/main/java/it/unibo/jurassiko/model/player/api/Player.java
@@ -2,6 +2,10 @@ package it.unibo.jurassiko.model.player.api;
 
 import java.util.Set;
 
+import it.unibo.jurassiko.model.objective.api.Objective;
+import it.unibo.jurassiko.model.territory.api.Ocean;
+import it.unibo.jurassiko.model.territory.api.Territory;
+
 /**
  * Interface who provide method for the Player
  */
@@ -35,50 +39,140 @@ public interface Player {
         }
 
         /**
+         * Get the color name.
+         * 
          * @return color name
          */
         public String getColor() {
             return color;
         }
+
+        /**
+         * Get the blue amount.
+         * 
+         * @return blue amount
+         */
+        public int getBlue() {
+            return blue;
+        }
+
+        /**
+         * Get the green amount.
+         * 
+         * @return green amount
+         */
+        public int getGreen() {
+            return green;
+        }
+
+        /**
+         * Get the red amount.
+         * 
+         * @return red amount
+         */
+        public int getRed() {
+            return red;
+        }
     }
 
     /**
+     * Get the player color.
+     * 
      * @return player color
      */
     Color getPlayerColor();
 
     /**
+     * Set the player objective.
+     * 
      * @param objective player objective
      */
     void setPlayerObjective(Objective objective);
 
     /**
+     * Get the player Objective.
+     * 
      * @return player objective
      */
     Objective getObjective();
 
     /**
-     * @return {@code true} if the objective is completed, {@code false} if otherwise
+     * @return {@code true} if the objective is completed, {@code false} if
+     *         otherwise
      */
     boolean isObjectiveComplete();
 
     /**
-     * @param territorie the territory to add
+     * Add a {@code Territory} to the player.
+     * 
+     * @param territory the territory to add
      */
-    void addPlayerTerritory(Territory territorie);
+    void addPlayerTerritory(Territory territory);
 
     /**
-     * @param territorie the territory to remove
+     * Remove a {@code Territory} to the player.
+     * 
+     * @param territory the territory to remove
      */
-    void removePlayerTerritory(Territory territorie);
+    void removePlayerTerritory(Territory territory);
 
     /**
+     * Get a Set of {@code Territory} owned by the player.
+     * 
      * @return set of player's territories
      */
     Set<Territory> getOwnedTerritories();
 
-    //Set<Continent> getOwnedContinents();
+    /**
+     * Add a {@code Ocean} to the player.
+     * 
+     * @param ocean the ocean to add
+     */
+    void addPlayerOcean(Ocean ocean);
 
-    // ??? getBonusHand();
+    /**
+     * Remove {@code Ocean} to the player.
+     * 
+     * @param ocean the ocean to remove
+     */
+    void removePlayerOcean(Ocean ocean);
+
+    /**
+     * Get a Set of {@code Ocean} owned by the player
+     * 
+     * @return set of player's oceans
+     */
+    Set<Ocean> getOwnedOceans();
+
+    /**
+     * Set the groundDino amount to add in each turn.
+     * 
+     * @param amount quantity of GroundDino to add each turn
+     */
+    void setBonusGroundDino(int amount);
+
+    /**
+     * Set the waterDino amount to add in each turn.
+     * 
+     * @param amount quantity of WaterDino to add each turn
+     */
+    void setBonusWaterDino(int amount);
+
+    /**
+     * Get the groundDino amount to add in each turn.
+     * 
+     * @return the player Ground Dino amount to add each turn
+     */
+    int getBonusGroundDino();
+
+    /**
+     * Get the waterDino amount to add in each turn.
+     * 
+     * @return the player Water Dino amount to add each turn
+     */
+    int getWaterDino();
+
+    // TODO:
+    // Set<Continent> getOwnedContinents();
 
 }
