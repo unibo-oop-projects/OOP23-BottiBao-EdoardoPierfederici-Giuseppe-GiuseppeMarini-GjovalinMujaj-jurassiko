c4de56c8af800074894147fe11d55e64f4879f75
Botti Bao <botti.bao@studio.unibo.it>
2024-01-31 23:01:51 +0100

    Add test for the player class (incomplete)

--- src/test/java/it/unibo/jurassiko/TestPlayer.java
+++ src/test/java/it/unibo/jurassiko/TestPlayer.java
@@ -1,5 +1,22 @@
 package it.unibo.jurassiko;
 
+import org.junit.jupiter.api.Test;
+
+import it.unibo.jurassiko.model.objective.impl.ConquerContinentsObjective;
+import it.unibo.jurassiko.model.player.api.Player;
+import it.unibo.jurassiko.model.player.impl.PlayerImpl;
+import it.unibo.jurassiko.model.territory.impl.OceanFactoryImpl;
+import it.unibo.jurassiko.model.territory.impl.TerritoryFactoryImpl;
+
 public class TestPlayer {
-    
+
+    private Player player;
+
+    @Test
+    void testTerritoriesAndOceans() {
+        player = new PlayerImpl(Player.Color.BLUE, new ConquerContinentsObjective(),
+                new TerritoryFactoryImpl().createTerritories(),
+                new OceanFactoryImpl().createOceans(), 0, 0);
+        
+    }
 }
