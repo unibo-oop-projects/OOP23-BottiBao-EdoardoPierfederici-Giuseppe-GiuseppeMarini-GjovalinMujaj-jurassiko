a70543aa39365e33249eb51d83238911b8d7e0ea
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2024-02-06 16:03:51 +0100

    Fix main class

--- src/main/java/it/unibo/jurassiko/Jurassiko.java
+++ src/main/java/it/unibo/jurassiko/Jurassiko.java
@@ -1,13 +1,14 @@
 package it.unibo.jurassiko;
 
+import it.unibo.jurassiko.view.gamescreen.impl.ViewImpl;
+
 public final class Jurassiko {
 
     private Jurassiko() {
-
+        
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        new ViewImpl();
     }
-
 }
