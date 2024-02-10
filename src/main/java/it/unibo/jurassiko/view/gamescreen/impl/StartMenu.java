89703a4dae968338543889540e9cf7dc20ccaaf5
Botti Bao <botti.bao@studio.unibo.it>
2024-02-10 18:24:58 +0100

    Remove unused imports

--- src/main/java/it/unibo/jurassiko/view/gamescreen/impl/StartMenu.java
+++ src/main/java/it/unibo/jurassiko/view/gamescreen/impl/StartMenu.java
@@ -1,19 +1,10 @@
 package it.unibo.jurassiko.view.gamescreen.impl;
 
-import it.unibo.jurassiko.controller.api.MenuController;
 import it.unibo.jurassiko.controller.impl.MenuContollerImpl;
 import it.unibo.jurassiko.view.gamescreen.api.View;
 import it.unibo.jurassiko.view.panel.MenuPanel;
 
-import java.awt.BorderLayout;
-import java.awt.Toolkit;
-
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
 /**
  * Graphical representation of the start Menu.
@@ -27,7 +18,7 @@ public class StartMenu extends JFrame implements View {
      */
     public StartMenu() {
         this.setTitle(TITLE);
-        
+
         this.setSize((int) ViewImpl.getScreenSize().getWidth() / 2, (int) ViewImpl.getScreenSize().getHeight() / 2);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(new MenuPanel(new MenuContollerImpl(), this));
