fb066e8a5e21a38ff45e7418322e2db16fcae72b
Botti Bao <botti.bao@studio.unibo.it>
2024-02-12 19:01:50 +0100

    Add test for gamePhase

---
+++ src/test/java/it/unibo/jurassiko/TestGamePhase.java
@@ -0,0 +1,30 @@
+package it.unibo.jurassiko;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.jurassiko.core.api.GamePhase;
+import it.unibo.jurassiko.core.impl.GamePhaseImpl;
+
+class TestGamePhase {
+
+    GamePhase gPhase;
+
+    @BeforeEach
+    void initPhase(){
+        gPhase = new GamePhaseImpl();
+    }
+    
+    @Test
+    void testPhases(){
+        assertEquals(gPhase.getPhase(), GamePhase.Phase.PLACEMENT);
+        gPhase.goNext();
+        assertEquals(gPhase.getPhase(), GamePhase.Phase.ATTACK);
+        gPhase.goNext();
+        assertEquals(gPhase.getPhase(), GamePhase.Phase.MOVEMENT);
+        gPhase.goNext();
+        assertEquals(gPhase.getPhase(), GamePhase.Phase.PLACEMENT);
+    }
+}
