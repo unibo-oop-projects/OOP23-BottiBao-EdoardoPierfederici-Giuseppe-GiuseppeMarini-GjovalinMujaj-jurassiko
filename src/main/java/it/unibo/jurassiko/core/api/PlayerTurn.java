6ebe272794e2a5ebfb4072f556465a5fd05ebecc
Botti Bao <botti.bao@studio.unibo.it>
2024-02-12 13:32:12 +0100

    Add PlayerTurn interface

---
+++ src/main/java/it/unibo/jurassiko/core/api/PlayerTurn.java
@@ -0,0 +1,26 @@
+package it.unibo.jurassiko.core.api;
+
+import java.util.List;
+
+import it.unibo.jurassiko.model.player.api.Player;
+
+public interface PlayerTurn {
+    /**
+     * get current Player Color in the turn.
+     * 
+     * @return Player Color
+     */
+    Player.GameColor getCurrentPlayerTurn();
+
+    /**
+     * get all the Players-
+     * 
+     * @return Set of all the Players
+     */
+    List<Player> getPlayers();
+
+    /**
+     * Finish the Turn of the vurrent Player and go to the next Player.
+     */
+    void goNext();
+}
