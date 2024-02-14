4e19db519e856b2253b53aa8f3310a4201d9235e
Botti Bao <botti.bao@studio.unibo.it>
2024-02-14 02:04:23 +0100

    Add methods in MainController and its implementation

--- src/main/java/it/unibo/jurassiko/controller/game/api/MainController.java
+++ src/main/java/it/unibo/jurassiko/controller/game/api/MainController.java
@@ -1,9 +1,11 @@
 package it.unibo.jurassiko.controller.game.api;
 
 import it.unibo.jurassiko.common.Pair;
+import it.unibo.jurassiko.core.api.GamePhase;
 import it.unibo.jurassiko.model.player.api.Player;
 import it.unibo.jurassiko.model.player.api.Player.GameColor;
 import it.unibo.jurassiko.model.territory.api.Territory;
+import it.unibo.jurassiko.view.window.TerritorySelector;
 
 import java.util.List;
 import java.util.Map;
@@ -14,12 +16,24 @@ public interface MainController {
      * 
      * @return map of the territories with the color and the amount of dino
      */
-    Map<Territory, Pair<GameColor,Integer>> getTerritoriesMap();
+    Map<Territory, Pair<GameColor, Integer>> getTerritoriesMap();
 
     List<Player> getPlayers() throws CloneNotSupportedException;
 
     void openTerritorySelector();
 
+    void closeTerritorySelector();
+
     void openView();
-    
+
+    GamePhase getGamePhase();
+
+    void addGroundDino(String territoryName, int amount);
+
+    void updateBoard();
+
+    TerritorySelector getTerritorySelector();
+
+    void startGameLoop();
+
 }
--- src/main/java/it/unibo/jurassiko/controller/game/impl/MainControllerImpl.java
+++ src/main/java/it/unibo/jurassiko/controller/game/impl/MainControllerImpl.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import it.unibo.jurassiko.common.Pair;
 import it.unibo.jurassiko.controller.game.api.MainController;
 import it.unibo.jurassiko.core.api.GameEngine;
+import it.unibo.jurassiko.core.api.GamePhase;
 import it.unibo.jurassiko.core.impl.GameEngineImpl;
 import it.unibo.jurassiko.model.objective.api.Objective;
 import it.unibo.jurassiko.model.objective.impl.ObjectiveFactoryImpl;
@@ -27,7 +28,7 @@ public class MainControllerImpl implements MainController {
     private final Set<Territory> allTerritories;
     private final Set<Ocean> oceans;
     private final Set<Objective> objectives;
-    private Map<Territory, Pair<GameColor,Integer>> mapTerritories;
+    private Map<Territory, Pair<GameColor, Integer>> mapTerritories;
     private Map<Ocean, GameColor> mapOcean;
 
     private final GameEngine game;
@@ -46,19 +47,23 @@ public class MainControllerImpl implements MainController {
         fullOcean();
         this.game = new GameEngineImpl(this);
         this.terrSelect = new TerritorySelector(this);
-        
+
     }
 
-    public void openTerritorySelector(){
+    public void openTerritorySelector() {
         this.terrSelect.display();
     }
 
-    public void openView(){
+    public void closeTerritorySelector() {
+        this.terrSelect.closeView();
+    }
+
+    public void openView() {
         mainFrame.display();
         updateBoard();
     }
 
-    public void updateBoard(){
+    public void updateBoard() {
         mainFrame.updatePanel();
     }
 
@@ -66,29 +71,32 @@ public class MainControllerImpl implements MainController {
         final var colorCurrentPlayer = this.game.getPlayerTurn().getCurrentPlayerTurn().getColor();
         switch (this.game.getGamePhase().getPhase()) {
             case PLACEMENT:
-                final Territory terr = this.mapTerritories.keySet().stream().filter(t -> t.getName().equals(territory)).findAny().get();
-                final int d = mapTerritories.get(terr).y();
-                mapTerritories.put(terr, new Pair<GameColor, Integer>(colorCurrentPlayer, d + 1));
+                /*
+                 * final Territory terr = getMapTerritoryKey(territory);
+                 * final int d = getMapTerritoryValue(territory).y();
+                 * mapTerritories.put(terr, new Pair<GameColor, Integer>(colorCurrentPlayer, d +
+                 * 1));
+                 */
+                // Same as AddGoundDino
                 updateBoard();
                 break;
             case ATTACK:
-                
+
                 break;
             case MOVEMENT:
-                
+
                 break;
             default:
                 break;
         }
     }
 
-
     public Set<Territory> getTerritories(final GameColor color) {
         final var setTerr = mapTerritories.entrySet()
-            .stream()
-            .filter(s -> s.getValue().x().equals(color))
-            .map(s -> s.getKey())
-            .collect(Collectors.toSet());
+                .stream()
+                .filter(s -> s.getValue().x().equals(color))
+                .map(s -> s.getKey())
+                .collect(Collectors.toSet());
         return setTerr;
     }
 
@@ -101,72 +109,113 @@ public class MainControllerImpl implements MainController {
         return players;
     }
 
-    public Map<Territory, Pair<GameColor,Integer>> getTerritoriesMap() {
+    public GamePhase getGamePhase() {
+        return game.getGamePhase();
+    }
+
+    public TerritorySelector getTerritorySelector() {
+        return terrSelect; // Penso che serve un copy nei metodi del territory Selector
+        // TODO: NOTA resetclick nella copia non va
+    }
+
+    public void addGroundDino(final String territoryName, final int amount) {
+        final var temp = getMapTerritoryValue(territoryName);
+        final var newPair = new Pair<>(temp.x(), temp.y() + amount);
+        mapTerritories.replace(getMapTerritoryKey(territoryName), newPair);
+    }
+
+    // given a name return the value
+    private Pair<GameColor, Integer> getMapTerritoryValue(final String territoryname) {
+        return mapTerritories.entrySet().stream()
+                .filter(t -> t.getKey().getName().equals(territoryname))
+                .map(t -> t.getValue())
+                .findFirst()
+                .get();
+    }
+
+    // given a name return the key
+    private Territory getMapTerritoryKey(final String territoryname) {
+        return mapTerritories.entrySet().stream()
+                .filter(t -> t.getKey().getName().equals(territoryname))
+                .map(t -> t.getKey())
+                .findFirst()
+                .get();
+    }
+
+    public Map<Territory, Pair<GameColor, Integer>> getTerritoriesMap() {
         return Map.copyOf(mapTerritories);
+    }
+
+    // TODO: NOTE TEMP, remove or mod if necessary
+    // its startPlacing for testing purpuse, change into gameloop
+    // when completed
+    public void startGameLoop() {
+        game.startGameLoop();
     }
 
     /**
      * Returns the color of the territory that we passed as input
      */
     private GameColor getColorTerritory(final Territory terr) {
-        if(redPlayer.getOwnedTerritories().contains(terr)){
+        if (redPlayer.getOwnedTerritories().contains(terr)) {
             return redPlayer.getColor();
         }
-        if(greenPlayer.getOwnedTerritories().contains(terr)){
+        if (greenPlayer.getOwnedTerritories().contains(terr)) {
             return greenPlayer.getColor();
         }
-        if(bluePlayer.getOwnedTerritories().contains(terr)){
+        if (bluePlayer.getOwnedTerritories().contains(terr)) {
             return bluePlayer.getColor();
         }
         throw new IllegalArgumentException("Territory not valid");
     }
-/* 
-    private GameColor getColorOcean(final Ocean oc) {
-        if(greenPlayer.getOwnedOceans().contains(oc)){
-            return greenPlayer.getColor();
-        }
-        if(bluePlayer.getOwnedOceans().contains(oc)){
-            return bluePlayer.getColor();
-        }
-        if(redPlayer.getOwnedOceans().contains(oc)){
-            return redPlayer.getColor();
-        }
-        throw new IllegalArgumentException("Ocean not valid");
-    }
-*/
+
+    /*
+     * private GameColor getColorOcean(final Ocean oc) {
+     * if(greenPlayer.getOwnedOceans().contains(oc)){
+     * return greenPlayer.getColor();
+     * }
+     * if(bluePlayer.getOwnedOceans().contains(oc)){
+     * return bluePlayer.getColor();
+     * }
+     * if(redPlayer.getOwnedOceans().contains(oc)){
+     * return redPlayer.getColor();
+     * }
+     * throw new IllegalArgumentException("Ocean not valid");
+     * }
+     */
     /**
-     * Create all the players using the methods that we have created, 
+     * Create all the players using the methods that we have created,
      * inside the constructor.
      */
     private void createPlayers() {
         final Set<Territory> copyTerritories = new HashSet<>(this.allTerritories);
         final Set<Objective> copyObjectives = new HashSet<>(this.objectives);
         this.redPlayer = new PlayerImpl(GameColor.RED,
-            shuffleObjective(copyObjectives),
-            shuffleTerritories(copyTerritories, MAX_TERRITORIES),
-            Set.of());
-        this.greenPlayer = new PlayerImpl(GameColor.GREEN, 
-            shuffleObjective(copyObjectives),
-            shuffleTerritories(copyTerritories, MAX_TERRITORIES),
-            Set.of());
-        this.bluePlayer = new PlayerImpl(GameColor.BLUE, 
-            shuffleObjective(copyObjectives),
-            shuffleTerritories(copyTerritories, MAX_TERRITORIES),
-            Set.of());
+                shuffleObjective(copyObjectives),
+                shuffleTerritories(copyTerritories, MAX_TERRITORIES),
+                Set.of());
+        this.greenPlayer = new PlayerImpl(GameColor.GREEN,
+                shuffleObjective(copyObjectives),
+                shuffleTerritories(copyTerritories, MAX_TERRITORIES),
+                Set.of());
+        this.bluePlayer = new PlayerImpl(GameColor.BLUE,
+                shuffleObjective(copyObjectives),
+                shuffleTerritories(copyTerritories, MAX_TERRITORIES),
+                Set.of());
     }
 
     /**
      * Shuffle all the territories and return 7 territories
      * for the current player.
      * 
-     * @param territories all territories in the game
+     * @param territories    all territories in the game
      * @param maxTerritories max territories for each player
      * @return a set of 7 territories
      */
     private Set<Territory> shuffleTerritories(final Set<Territory> territories, final int maxTerritories) {
         final Set<Territory> temp = territories.stream()
-            .limit(maxTerritories)
-            .collect(Collectors.toSet());
+                .limit(maxTerritories)
+                .collect(Collectors.toSet());
         territories.removeAll(temp);
         return temp;
     }
@@ -183,20 +232,21 @@ public class MainControllerImpl implements MainController {
         objective.remove(temp);
         return temp;
     }
-/* 
-    private Set<Ocean> shuffleOcean(final Set<Ocean> oceans) {
-        final Set<Ocean> temp = oceans.stream().findFirst().stream().collect(Collectors.toSet());
-        oceans.removeAll(temp);
-        return temp;
-    }
-*/
+
+    /*
+     * private Set<Ocean> shuffleOcean(final Set<Ocean> oceans) {
+     * final Set<Ocean> temp =
+     * oceans.stream().findFirst().stream().collect(Collectors.toSet());
+     * oceans.removeAll(temp);
+     * return temp;
+     * }
+     */
     /**
      * Full the mapOcean only with the oceans
      */
     private void fullOcean() {
         mapOcean = new HashMap<>();
-        oceans.stream().forEach(ocean -> 
-            mapOcean.put(ocean, null));
+        oceans.stream().forEach(ocean -> mapOcean.put(ocean, null));
     }
 
     /**
@@ -205,8 +255,8 @@ public class MainControllerImpl implements MainController {
      */
     private void fullTerritories() {
         mapTerritories = new HashMap<>();
-        allTerritories.stream().forEach(terr -> 
-            mapTerritories.put(terr, new Pair<>(getColorTerritory(terr), START_AMOUNT_DINO)));
+        allTerritories.stream()
+                .forEach(terr -> mapTerritories.put(terr, new Pair<>(getColorTerritory(terr), START_AMOUNT_DINO)));
     }
 
 }
