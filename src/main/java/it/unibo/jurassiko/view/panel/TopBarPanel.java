e1462ec83f6a3c0384072704fdc0b404d9f72a98
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2024-02-14 14:31:34 +0100

    Add PlayerLabel

--- src/main/java/it/unibo/jurassiko/view/panel/TopBarPanel.java
+++ src/main/java/it/unibo/jurassiko/view/panel/TopBarPanel.java
@@ -1,8 +1,11 @@
 package it.unibo.jurassiko.view.panel;
 
+import it.unibo.jurassiko.controller.game.api.MainController;
+import it.unibo.jurassiko.model.player.api.Player.GameColor;
 import it.unibo.jurassiko.view.gamescreen.impl.ViewImpl;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -29,13 +32,16 @@ public class TopBarPanel extends JPanel {
     private static final int FONT_SIZE = 24;
     private static final int DISTANCE_BUTTON_L_R = 48;
     private static final String URL_IMAGE = "images/topbar.png";
-
+    private final MainController main;
     private final JLabel topLabel;
+    private JLabel currentPlayer;
+    private GameColor currentColor;
     /**
      * Set the top-bar in the relevant label load the buttons in it,
      * and add all to the relevant panel.
      */
-    public TopBarPanel() {
+    public TopBarPanel(MainController main) {
+        this.main = main;
         BufferedImage imageBar;
         try {
             imageBar = ImageIO.read(ClassLoader.getSystemResource(URL_IMAGE));
@@ -61,16 +67,38 @@ public class TopBarPanel extends JPanel {
         final JButton objective = new JButton("Obiettivo");
         final JButton attack = new JButton("Attacco");
         final JButton endTurn = new JButton("Fine turno");
-        final Font fontButton = new Font("Serif", Font.BOLD, FONT_SIZE);
-        objective.setFont(fontButton);
-        attack.setFont(fontButton);
-        endTurn.setFont(fontButton);
+        this.currentColor = this.main.getCurrentPlayer().getColor();
+        this.currentPlayer = new JLabel("Player" + currentColor.getColor());
+        final Font font = new Font("Serif", Font.BOLD, FONT_SIZE);
+        objective.setFont(font);
+        attack.setFont(font);
+        endTurn.setFont(font);
+        currentPlayer.setFont(font);
         final Insets insets = new Insets(8, DISTANCE_BUTTON_L_R, 8, DISTANCE_BUTTON_L_R);
-        this.topLabel.add(objective,
+        this.topLabel.add(currentPlayer,
             new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
-        this.topLabel.add(attack, 
+        this.topLabel.add(objective,
             new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
-        this.topLabel.add(endTurn, 
+        this.topLabel.add(attack, 
             new GridBagConstraints(2, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
+        this.topLabel.add(endTurn, 
+            new GridBagConstraints(3, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
+    }
+
+    private Color getLabelColor(GameColor color) {
+        return switch (color) {
+            case RED -> Color.RED;
+            case GREEN -> Color.GREEN;
+            case BLUE -> Color.BLUE;
+            default -> throw new IllegalArgumentException("Invalid color");
+        };
+    }
+
+    public void setColorPlayer(){
+        currentPlayer.setForeground(getLabelColor(currentColor));
+    }
+
+    public void updateTopBar(){
+        setColorPlayer();
     }
 }
