0b526d54ff045cbd7b02b0d90ac788359ca50ed1
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-02-14 15:07:49 +0100

    Fix currentPlayer setter

--- src/main/java/it/unibo/jurassiko/view/panel/TopBarPanel.java
+++ src/main/java/it/unibo/jurassiko/view/panel/TopBarPanel.java
@@ -6,6 +6,7 @@ import it.unibo.jurassiko.view.gamescreen.impl.ViewImpl;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -31,11 +32,12 @@ public class TopBarPanel extends JPanel {
     private static final double WIDTH_RATIO = 0.8;
     private static final int FONT_SIZE = 24;
     private static final int DISTANCE_BUTTON_L_R = 48;
+    private static final int BG_PLAYER_RGB = 175;
     private static final String URL_IMAGE = "images/topbar.png";
     private final MainController main;
     private final JLabel topLabel;
     private JLabel currentPlayer;
-    private GameColor currentColor;
+
     /**
      * Set the top-bar in the relevant label load the buttons in it,
      * and add all to the relevant panel.
@@ -53,7 +55,7 @@ public class TopBarPanel extends JPanel {
         final ImageIcon imageTopBar = ViewImpl.scaleImage(imageBar, width, height);
         this.topLabel = new JLabel(imageTopBar);
         this.topLabel.setLayout(new GridBagLayout());
-        this.loadButton();
+        this.loadLabel();
         this.topLabel.setBounds(0, 0, width, height);
         this.setLayout(new BorderLayout());
         this.add(topLabel);
@@ -63,26 +65,32 @@ public class TopBarPanel extends JPanel {
     /**
      * Load the button in the relevant label.
      */
-    private void loadButton() {
+    private void loadLabel() {
         final JButton objective = new JButton("Obiettivo");
         final JButton attack = new JButton("Attacco");
         final JButton endTurn = new JButton("Fine turno");
-        this.currentColor = this.main.getCurrentPlayer().getColor();
-        this.currentPlayer = new JLabel("Player" + currentColor.getColor());
+        this.currentPlayer = new JLabel();
+        this.currentPlayer.setBackground(new Color(BG_PLAYER_RGB, BG_PLAYER_RGB, BG_PLAYER_RGB));
+        this.currentPlayer.setOpaque(true);
+        setCurrentPlayer();
+
         final Font font = new Font("Serif", Font.BOLD, FONT_SIZE);
         objective.setFont(font);
         attack.setFont(font);
         endTurn.setFont(font);
         currentPlayer.setFont(font);
+
+        addComponent(currentPlayer, 0, 0);
+        addComponent(objective, 1, 0);
+        addComponent(attack, 2, 0);
+        addComponent(endTurn, 3, 0);
+    }
+
+    private void addComponent(Component component, int gridx, int gridy) {
         final Insets insets = new Insets(8, DISTANCE_BUTTON_L_R, 8, DISTANCE_BUTTON_L_R);
-        this.topLabel.add(currentPlayer,
-            new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
-        this.topLabel.add(objective,
-            new GridBagConstraints(1, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
-        this.topLabel.add(attack, 
-            new GridBagConstraints(2, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
-        this.topLabel.add(endTurn, 
-            new GridBagConstraints(3, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER, insets, 0, 0));
+        var gbc = new GridBagConstraints(gridx, gridy, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER,
+                insets, 0, 0);
+        this.topLabel.add(component, gbc);
     }
 
     private Color getLabelColor(GameColor color) {
@@ -94,11 +102,13 @@ public class TopBarPanel extends JPanel {
         };
     }
 
-    public void setColorPlayer(){
-        currentPlayer.setForeground(getLabelColor(currentColor));
+    public void setCurrentPlayer() {
+        var currentColor = this.main.getCurrentPlayer().getColor();
+        this.currentPlayer.setForeground(getLabelColor(currentColor));
+        this.currentPlayer.setText("Player: " + currentColor.getColor()); // TODO: change method name
     }
 
-    public void updateTopBar(){
-        setColorPlayer();
+    public void updateTopBar() {
+        setCurrentPlayer();
     }
 }
