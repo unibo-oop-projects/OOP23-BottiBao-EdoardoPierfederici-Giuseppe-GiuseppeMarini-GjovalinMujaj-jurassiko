571ec2659cc597bcaabae43a585d507feb62dafb
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-02-15 10:49:33 +0100

    Fix end turn dialog, other minor fixes

--- src/main/java/it/unibo/jurassiko/view/panel/TopBarPanel.java
+++ src/main/java/it/unibo/jurassiko/view/panel/TopBarPanel.java
@@ -30,13 +30,18 @@ import javax.swing.JPanel;
 public class TopBarPanel extends JPanel {
 
     private static final long serialVersionUID = 1379037036670658465L;
-
     private static final double HEIGHT_RATIO = 0.1;
     private static final double WIDTH_RATIO = 0.8;
     private static final int FONT_SIZE = 24;
     private static final int DISTANCE_BUTTON_L_R = 48;
     private static final int BG_PLAYER_RGB = 175;
     private static final String URL_IMAGE = "images/topbar.png";
+    private static final String OBJ_BUTTON_NAME = "Obiettivo";
+    private static final String PLACE_BUTTON_NAME = "Piazzamento";
+    private static final String ATK_BUTTON_NAME = "Attacco";
+    private static final String ENDTURN_BUTTON_NAME = "Fine Turno";
+    private static final String ENDTURN_DIALOG_QUESTION = "Vuoi effettuare uno spostamento prima di terminare il turno?";
+
     private final MainController controller;
     private final JLabel topLabel;
     private JLabel currentPlayer;
@@ -53,21 +58,23 @@ public class TopBarPanel extends JPanel {
     public TopBarPanel(final MainController controller, final ObjectiveWindow objectiveCard) {
         this.objectiveCard = objectiveCard;
         this.controller = controller;
+
         BufferedImage imageBar;
         try {
             imageBar = ImageIO.read(ClassLoader.getSystemResource(URL_IMAGE));
         } catch (final IOException e) {
             throw new IllegalStateException("Failed to read the top bar file", e);
         }
+
         final int width = (int) (WIDTH_RATIO * ViewImpl.getScreenSize().getWidth());
         final int height = (int) (HEIGHT_RATIO * ViewImpl.getScreenSize().getHeight());
         final ImageIcon imageTopBar = ViewImpl.scaleImage(imageBar, width, height);
         this.topLabel = new JLabel(imageTopBar);
         this.topLabel.setLayout(new GridBagLayout());
-        this.objective = new JButton("Obiettivo");
-        this.place = new JButton("Piazza");
-        this.attack = new JButton("Attacco");
-        this.endTurn = new JButton("Fine turno");
+        this.objective = new JButton(OBJ_BUTTON_NAME);
+        this.place = new JButton(PLACE_BUTTON_NAME);
+        this.attack = new JButton(ATK_BUTTON_NAME);
+        this.endTurn = new JButton(ENDTURN_BUTTON_NAME);
         this.loadLabel();
         this.topLabel.setBounds(0, 0, width, height);
         this.setLayout(new BorderLayout());
@@ -80,22 +87,23 @@ public class TopBarPanel extends JPanel {
      * Load the button in the relevant label.
      */
     private void loadLabel() {
-
         this.objective.addActionListener(e -> this.objectiveCard.showObjectiveCard());
         this.place.addActionListener(e -> this.controller.startGameLoop());
         // TODO: attack actionListener
         this.endTurn.addActionListener(e -> {
-            final String[] options = { "Yes", "Movement" };
+            final String[] options = { "Sì", "No" };
             final int result = JOptionPane.showOptionDialog(this,
-                    "End Turn",
-                    "Premere \"Yes\" per finire il turno\nMovement per entrare nella fase di Movimento",
+                    ENDTURN_DIALOG_QUESTION,
+                    ENDTURN_BUTTON_NAME,
                     JOptionPane.YES_NO_OPTION,
-                    JOptionPane.WARNING_MESSAGE,
-                    null, options, options);
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    options,
+                    0);
             switch (result) {
-                case 0 -> controller.endTurn();
-                case 1 -> controller.setGamePhase(Phase.MOVEMENT);
-                default -> throw new IllegalArgumentException("How?");
+                case 0 -> controller.setGamePhase(Phase.MOVEMENT);
+                case 1 -> controller.endTurn();
+                default -> throw new IllegalArgumentException("Invalid option");
             }
         });
         this.currentPlayer = new JLabel();
@@ -104,11 +112,11 @@ public class TopBarPanel extends JPanel {
         setCurrentPlayer();
 
         final Font font = new Font("Serif", Font.BOLD, FONT_SIZE);
-        objective.setFont(font);
-        place.setFont(font);
-        attack.setFont(font);
-        endTurn.setFont(font);
-        currentPlayer.setFont(font);
+        this.objective.setFont(font);
+        this.place.setFont(font);
+        this.attack.setFont(font);
+        this.endTurn.setFont(font);
+        this.currentPlayer.setFont(font);
 
         addComponent(currentPlayer, 0, 0);
         addComponent(objective, 1, 0);
@@ -119,8 +127,8 @@ public class TopBarPanel extends JPanel {
 
     private void addComponent(final Component component, final int gridx, final int gridy) {
         final Insets insets = new Insets(8, DISTANCE_BUTTON_L_R, 8, DISTANCE_BUTTON_L_R);
-        final var gbc = new GridBagConstraints(gridx, gridy, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.CENTER,
-                insets, 0, 0);
+        final var gbc = new GridBagConstraints(gridx, gridy, 1, 1, 0, 0, GridBagConstraints.CENTER,
+                GridBagConstraints.CENTER, insets, 0, 0);
         this.topLabel.add(component, gbc);
     }
 
@@ -142,13 +150,13 @@ public class TopBarPanel extends JPanel {
     private void setActiveButton() {
         final var phase = controller.getGamePhase();
         disableAllJButtons();
-        if (phase.equals(Phase.PLACEMENT)) {
-            place.setEnabled(true);
-        } else if (phase.equals(Phase.ATTACK)) {
-            attack.setEnabled(true);
-            endTurn.setEnabled(true);
-        } else if (phase.equals(Phase.MOVEMENT)) {
-            endTurn.setEnabled(true);
+        switch (phase) {
+            case PLACEMENT -> this.place.setEnabled(true);
+            case ATTACK -> {
+                this.attack.setEnabled(true);
+                this.endTurn.setEnabled(true);
+            }
+            case MOVEMENT -> this.endTurn.setEnabled(true);
         }
     }
 
