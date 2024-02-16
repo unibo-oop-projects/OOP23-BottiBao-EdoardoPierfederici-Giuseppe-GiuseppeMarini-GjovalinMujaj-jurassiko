f0020ce8dfed0c5c8622f18fdc107caaef79eb4d
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-02-16 19:07:50 +0100

    Fix some sprite positions

--- src/main/java/it/unibo/jurassiko/view/panel/SpriteLoader.java
+++ src/main/java/it/unibo/jurassiko/view/panel/SpriteLoader.java
@@ -23,8 +23,10 @@ public class SpriteLoader {
     private static final String URL_REDLAPRAS = "images/sprites/laprasred.png";
     private static final String URL_BLUELAPRAS = "images/sprites/laprasblue.png";
     private static final String URL_GREENLAPRAS = "images/sprites/laprasgreen.png";
-    private static final double HEIGHT_RATIO = 0.05;
-    private static final double WIDTH_RATIO = 0.025;
+    private static final double DINO_WIDTH_RATIO = 0.025;
+    private static final double DINO_HEIGHT_RATIO = 0.05;
+    private static final double LAPRAS_WIDTH_RATIO = 0.04;
+    private static final double LAPRAS_HEIGHT_RATIO = 0.08;
 
     private final Map<GameColor, ImageIcon> dinoSprites;
     private final Map<GameColor, ImageIcon> laprasSprites;
@@ -52,23 +54,25 @@ public class SpriteLoader {
         } catch (final IOException e) {
             throw new IllegalStateException("Failed to read sprite files", e);
         }
-        final int width = (int) (WIDTH_RATIO * ViewImpl.getScreenSize().getWidth());
-        final int height = (int) (HEIGHT_RATIO * ViewImpl.getScreenSize().getHeight());
+        final int dinoWidth = (int) (DINO_WIDTH_RATIO * ViewImpl.getScreenSize().getWidth());
+        final int dinoHeight = (int) (DINO_HEIGHT_RATIO * ViewImpl.getScreenSize().getHeight());
+        final int laprasWidth = (int) (LAPRAS_WIDTH_RATIO * ViewImpl.getScreenSize().getWidth());
+        final int laprasHeight = (int) (LAPRAS_HEIGHT_RATIO * ViewImpl.getScreenSize().getHeight());
 
         this.dinoSprites.put(GameColor.RED,
-                ViewImpl.scaleImage(redDino, width, height));
+                ViewImpl.scaleImage(redDino, dinoWidth, dinoHeight));
         this.dinoSprites.put(
-                GameColor.BLUE, ViewImpl.scaleImage(blueDino, width, height));
+                GameColor.BLUE, ViewImpl.scaleImage(blueDino, dinoWidth, dinoHeight));
         this.dinoSprites.put(
-                GameColor.GREEN, ViewImpl.scaleImage(greenDino, width, height));
+                GameColor.GREEN, ViewImpl.scaleImage(greenDino, dinoWidth, dinoHeight));
         this.laprasSprites.put(GameColor.DEFAULT,
-                ViewImpl.scaleImage(baseLapras, width, height));
+                ViewImpl.scaleImage(baseLapras, laprasWidth, laprasHeight));
         this.laprasSprites.put(GameColor.RED,
-                ViewImpl.scaleImage(redLapras, width, height));
+                ViewImpl.scaleImage(redLapras, laprasWidth, laprasHeight));
         this.laprasSprites.put(GameColor.BLUE,
-                ViewImpl.scaleImage(blueLapras, width, height));
+                ViewImpl.scaleImage(blueLapras, laprasWidth, laprasHeight));
         this.laprasSprites.put(GameColor.GREEN,
-                ViewImpl.scaleImage(greenLapras, width, height));
+                ViewImpl.scaleImage(greenLapras, laprasWidth, laprasHeight));
     }
 
     /**
