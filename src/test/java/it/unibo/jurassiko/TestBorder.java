be0584f290d41c306a7776d56ff1579544828f3f
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2024-02-17 15:32:06 +0100

    Fix testBorder

--- src/test/java/it/unibo/jurassiko/TestBorder.java
+++ src/test/java/it/unibo/jurassiko/TestBorder.java
@@ -24,7 +24,7 @@ class TestBorder {
     private static final String TERRITORY_NAME_1 = "Madagascar";
     private static final String OCEAN_NAME_1 = "Oceano Tetide";
     private static final String OCEAN_NAME_2 = "Oceano Atlantico";
-    private static final Set<String> NEIGHBOUR_NAMES = Set.of("India", "Antartica");
+    private static final Set<String> NEIGHBOUR_NAMES = Set.of("India", "Antartica", "Australia");
     private static final Set<String> NEIGHBOUR_NAME_2 = Set.of("Tibet", "Indonesia", "Nord Africa",
             "Congo", "Sud Africa", "Australia", "India", "Madagascar", "Antartica");
 
