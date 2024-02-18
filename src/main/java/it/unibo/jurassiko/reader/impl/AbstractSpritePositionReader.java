f559c720022605ec79f73c341fd118228db7414a
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-02-18 10:32:54 +0100

    Add abstract class for sprite position readers

---
+++ src/main/java/it/unibo/jurassiko/reader/impl/AbstractSpritePositionReader.java
@@ -0,0 +1,61 @@
+package it.unibo.jurassiko.reader.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import it.unibo.jurassiko.common.Pair;
+import it.unibo.jurassiko.reader.api.JSONFileReader;
+
+/**
+ * Abstract class providing a common implementation to read from a JSON file the
+ * coordinates of the sprites for the game board, calculated as a percentage of
+ * the board panel dimension.
+ */
+public abstract class AbstractSpritePositionReader implements JSONFileReader<Map<String, Pair<Double, Double>>> {
+
+    private final ObjectMapper mapper;
+
+    /**
+     * Creates an AbstractSpritePositionReader.
+     */
+    public AbstractSpritePositionReader() {
+        this.mapper = new ObjectMapper();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Map<String, Pair<Double, Double>> readFileData(final String filePath) {
+        final Map<String, Pair<Double, Double>> data = new HashMap<>();
+
+        try (InputStream in = Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(filePath))) {
+            final JsonNode jsonNode = this.mapper.readTree(in);
+            jsonNode.forEach(t -> {
+                final String spriteName = getSpriteName(t);
+                final double x = t.get("x").asDouble();
+                final double y = t.get("y").asDouble();
+                data.put(spriteName, new Pair<>(x, y));
+            });
+        } catch (final IOException e) {
+            throw new IllegalStateException("Failed to read " + filePath + " file", e);
+        }
+
+        return Map.copyOf(data);
+    }
+
+    /**
+     * Reads the type of the sprite from the given Jackson jsonNode.
+     * 
+     * @param jsonNode the jsonNode used to parse data
+     * @return the name of the sprite type
+     */
+    protected abstract String getSpriteName(JsonNode jsonNode);
+
+}
