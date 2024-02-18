a5ba5b00d2f0686e5d2ca484da1931f8c3eb226d
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2024-02-18 22:17:04 +0100

    Remove unused import

--- src/main/java/it/unibo/jurassiko/core/impl/GameEngineImpl.java
+++ src/main/java/it/unibo/jurassiko/core/impl/GameEngineImpl.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.jurassiko.Jurassiko;
 import it.unibo.jurassiko.controller.api.MainController;
 import it.unibo.jurassiko.core.api.GameEngine;
 import it.unibo.jurassiko.core.api.GamePhase;
